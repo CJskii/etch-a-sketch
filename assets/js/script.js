f16a0c73e7aac1593fa302941f51e8751fa25572
CJskii <czareczarek3@gmail.com>
2022-07-29 23:42:58 +0100

    grid not finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,17 +21,20 @@ function logText(e){
 
 
 let makeGrid = function(rows, cols){
-    gridContainer.cssText = `grid-template-columns: repeat(${rows}, 25px [col-start]); display: grid;`
+    gridContainer.cssText = `grid-template-columns: repeat(${rows}, 1fr); 
+    grid-template-rows: repeat(${cols}, 1fr); grid-column-gap: 0px;
+    grid-row-gap: 0px; display: grid;`
     console.log(gridContainer.cssText)
     for (let i = 1; i <= (rows * cols); i++){
         let div = document.createElement('div')
-        div.setAttribute('class', 'col-1 align-self-stretch')
-        div.style.cssText = "border: 1px solid black; height: 25px; width: 25px"; 
+        div.setAttribute('class', 'col-1 grid-item')
+        div.style.cssText = "border: 1px solid black; height: 36px; width: 36px"; 
         gridContainer.appendChild(div);
     };
 };
 
 
+/* TABLET div.style.cssText = "border: 1px solid black; height: 45px; width: 45px";  */
 
 /*
 function colorSelection
